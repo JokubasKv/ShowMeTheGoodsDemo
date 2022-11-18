d071863607c935fa8d83ca385129fe373408f067
JokubasKv <jokubaskv@gmail.com>
2022-11-18 02:08:14 +0200

    fix a secret leak

--- ShowMeTheGoodsDemo/Data/ShowMeTheGoodsDbContext.cs
+++ ShowMeTheGoodsDemo/Data/ShowMeTheGoodsDbContext.cs
@@ -22,11 +22,7 @@ namespace ShowMeTheGoodsDemo.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
            // string s = _configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
-            optionsBuilder.UseSqlServer("Data Source = tcp:showmethegoodsdemodbserver.database.windows.net,1433; Initial Catalog = ShowMeTheGoodsDemo_db; User Id = jokkve@showmethegoodsdemodbserver; Password = Burokas456.");
+            optionsBuilder.UseSqlServer("");
         }
     }
 }
-//"Server=tcp:showmethegoodsdemodbserver.database.windows.net,1433;Initial Catalog=ShowMeTheGoodsDemo_db;Persist Security Info=False;User ID=jokkve;Password=Burokas456.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-//
-//"Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=ShowMeTheGoodDb"
-//Data Source=showmethegoodsdemodbserver.database.windows.net,1433;Initial Catalog=ShowMeTheGoodsDemo_db;User ID=jokkve;Password=Burokas456.
